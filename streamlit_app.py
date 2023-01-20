48fbc64bf01c836c5cba21ecc6e621672817d03d
pavanundamattla <123149631+pavanundamattla@users.noreply.github.com>
2023-01-20 12:50:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title ("My Parents New Healthy Diner")
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text("omega 3 & blueberry oatmeal")
+streamlit.text("kale,spinach")
+streamlit.text("Hard boiled Free-range Egg")
